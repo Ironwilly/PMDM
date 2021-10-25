d4c52bd2f5ab9b9f30de30beb3db2ba37cbb675d
Guillermo <claseswilly@gmail.com>
2021-10-26 01:24:17 +0200

    Casa 25 octubre 2021

---
+++ angular-pokemon/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialImportsModule } from './modules/material-imports.module';
+import { FormsModule } from '@angular/forms';
+import { PokemonItemComponent } from './pokemon-item/pokemon-item.component';
+import { PokemonCardComponent } from './pokemon-card/pokemon-card/pokemon-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PokemonListComponent,
+    PokemonItemComponent,
+    PokemonCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialImportsModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
